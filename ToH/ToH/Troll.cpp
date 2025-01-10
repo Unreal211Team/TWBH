9223ed52b51906ac2747deb5ba3a5e50c70d65d8
Minwoo <ymw1023@kakao.com>
2025-01-10 21:12:20 +0900

    ✨ Update : Troll class : Functions
    디폴트 매개인자를 갖는 생성자가 생겼습니다.
    getName 함수를 정의했습니다.
    getHealth 함수를 정의했습니다.
    getAttack 함수를 정의했습니다.
    takeDamage 함수를 정의했습니다.

--- ToH/ToH/Troll.cpp
+++ ToH/ToH/Troll.cpp
@@ -1,1 +1,37 @@
 #include "Troll.h"
+#include <random>
+#include <iostream>
+
+Troll::Troll(int level)
+{
+	random_device rd;
+	uniform_int_distribution<int> randomHealth(20, 30);
+	uniform_int_distribution<int> randomAttack(5, 10);
+
+	static int count = 1;
+
+	name = "troll" + to_string(count++);
+	health = level * randomHealth(rd);
+	attack = level * randomAttack(rd);
+}
+
+string Troll::getName() const
+{
+	return name;
+}
+
+int Troll::getHealth() const
+{
+	return health;
+}
+
+int Troll::getAttack() const
+{
+	return attack;
+}
+
+void Troll::takeDamage(int damage)
+{
+	health -= damage;
+	health = health < 0 ? 0 : health;
+}
