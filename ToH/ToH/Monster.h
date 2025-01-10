e5f1bb522caa87298c0f7cc9f24889a9feb6bdc1
Minwoo <ymw1023@kakao.com>
2025-01-10 19:22:31 +0900

    ✨ Update : Monster class : Functions
    getName 함수가 생겼습니다.
    getHealth 함수가 생겼습니다.
    getAttack 함수가 생겼습니다.
    takeDamage 함수가 생겼습니다.

--- ToH/ToH/Monster.h
+++ ToH/ToH/Monster.h
@@ -1,6 +1,14 @@
 #pragma once
 
+#include <string>
+
+using namespace std;
+
 class Monster
 {
-
+public:
+	virtual string	getName() const = 0;
+	virtual int		getHealth() const = 0;
+	virtual	int		getAttack() const = 0;
+	virtual void	takeDamage(int damage) = 0;
 };
