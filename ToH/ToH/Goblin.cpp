41a2f44a9db3fef688962dbf8cc7cb8f5156ecd2
Minwoo <ymw1023@kakao.com>
2025-01-10 20:47:25 +0900

    ✨ Update : Goblin class : Functions
    디폴트 매개인자를 갖는 생성자가 생겼습니다.
    getName 함수를 정의했습니다.
    getHealth 함수를 정의했습니다.
    getAttack 함수를 정의했습니다.
    takeDamage 함수를 정의했습니다.

--- ToH/ToH/Goblin.cpp
+++ ToH/ToH/Goblin.cpp
@@ -1,1 +1,42 @@
 #include "Goblin.h"
+#include <random>
+#include <iostream>
+
+Goblin::Goblin(int level)
+{
+	random_device rd;
+	uniform_int_distribution<int> randomHealth(20, 30);
+	uniform_int_distribution<int> randomAttack(5, 10);
+
+	static int count = 1;
+
+	name = "goblin" + to_string(count++);
+	health = level * randomHealth(rd);
+
+	attack = level * randomAttack(rd);
+
+	cout << name << endl;
+	cout << health << endl;
+	cout << attack << endl;
+}
+
+string Goblin::getName() const
+{
+	return name;
+}
+
+int Goblin::getHealth() const
+{
+	return health;
+}
+
+int Goblin::getAttack() const
+{
+	return attack;
+}
+
+void Goblin::takeDamage(int damage)
+{
+	health -= damage;
+	health = health < 0 ? 0 : health;
+}
