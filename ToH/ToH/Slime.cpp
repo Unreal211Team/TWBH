92975040472f57a3ff2eb9916f781372d549e0dd
Minwoo <ymw1023@kakao.com>
2025-01-10 21:30:10 +0900

    ✨ Update : Slime class : Functions
    디폴트 매개인자를 갖는 생성자가 생겼습니다.
    getName 함수를 정의했습니다.
    getHealth 함수를 정의했습니다.
    getAttack 함수를 정의했습니다.
    takeDamage 함수를 정의했습니다.

--- ToH/ToH/Slime.cpp
+++ ToH/ToH/Slime.cpp
@@ -1,1 +1,36 @@
 #include "Slime.h"
+#include <random>
+
+Slime::Slime(int level)
+{
+	random_device rd;
+	uniform_int_distribution<int> randomHealth(20, 30);
+	uniform_int_distribution<int> randomAttack(5, 10);
+
+	static int count = 1;
+
+	name = "slime" + to_string(count++);
+	health = level * randomHealth(rd);
+	attack = level * randomAttack(rd);
+}
+
+string Slime::getName() const
+{
+	return name;
+}
+
+int Slime::getHealth() const
+{
+	return health;
+}
+
+int Slime::getAttack() const
+{
+	return attack;
+}
+
+void Slime::takeDamage(int damage)
+{
+	health -= damage;
+	health = health < 0 ? 0 : health;
+}
