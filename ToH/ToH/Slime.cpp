cd5f7530643904cb2024672ede6d6a3e4eb827eb
Minwoo <ymw1023@kakao.com>
2025-01-13 16:42:10 +0900

    ✨ Update : Slime class : Function
    dropItem 함수를 정의했습니다.

--- ToH/ToH/Slime.cpp
+++ ToH/ToH/Slime.cpp
@@ -1,15 +1,19 @@
 #include "Slime.h"
 #include <random>
+#include "SlimeDropItem.h"
+#include <algorithm>  // max() 함수 사용을 위해 필요
+
+using namespace std;
 
 Slime::Slime(int level)
 {
 	random_device rd;
-	uniform_int_distribution<int> randomHealth(20, 30);
-	uniform_int_distribution<int> randomAttack(5, 10);
+	uniform_int_distribution<int> randomHealth(20, 30);	// 체력 랜덤 범위
+	uniform_int_distribution<int> randomAttack(5, 10);	// 공격력 랜덤 범위
 
 	static int count = 1;
 
-	name = "slime" + to_string(count++);
+	name = "Slime" + to_string(count++);
 	health = level * randomHealth(rd);
 	attack = level * randomAttack(rd);
 }
@@ -31,6 +35,11 @@ int Slime::getAttack() const
 
 void Slime::takeDamage(int damage)
 {
-	health -= damage;
-	health = health < 0 ? 0 : health;
+	health = max(0, health - damage);
+}
+
+Item* Slime::dropItem() const
+{
+	Item* drop = new SlimeDropItem();
+	return drop;
 }
