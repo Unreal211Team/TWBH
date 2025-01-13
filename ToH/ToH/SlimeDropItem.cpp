52554aa3e6805bd12804a8da47d1596829b9256b
Minwoo <ymw1023@kakao.com>
2025-01-13 17:05:13 +0900

    ✨Update : SlimeDropItem class : Functions
    디폴트 매개인자를 갖는 생성자가 생겼습니다.
    getName 함수를 정의했습니다.
    canUse 함수를 정의했습니다.
    use 함수를 정의했습니다.
    getPrice 함수를 정의했습니다.

--- ToH/ToH/SlimeDropItem.cpp
+++ ToH/ToH/SlimeDropItem.cpp
@@ -1,1 +1,29 @@
 #include "SlimeDropItem.h"
+#include "Character.h"
+
+using namespace std;
+
+SlimeDropItem::SlimeDropItem(string name, bool canUsing, int price)
+	: name(name), canUsing(canUsing), price(price)
+{
+}
+
+std::string SlimeDropItem::getName() const
+{
+	return name;
+}
+
+bool SlimeDropItem::canUse() const
+{
+	return canUsing;
+}
+
+void SlimeDropItem::use(Character* character) const
+{	// 사용가능한 아이템이 아님
+	return;
+}
+
+int SlimeDropItem::getPrice() const
+{
+	return price;
+}
--- ToH/ToH/SlimeDropItem.h
+++ ToH/ToH/SlimeDropItem.h
@@ -8,4 +8,11 @@ private:
 	const std::string name;
 	const bool canUsing;
 	const int price;
+
+public:
+	SlimeDropItem(std::string name = "불투명한 슬라임의 점액", bool canUsing = false, int price = 200);
+	std::string getName() const override;
+	bool canUse() const override;
+	void use(Character* character) const override;
+	int getPrice() const override;
 };
