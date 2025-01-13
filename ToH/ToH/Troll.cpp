17efc16f42d5e6f815716c2f2e80e32e84840a4a
Minwoo <ymw1023@kakao.com>
2025-01-13 16:39:44 +0900

    ✨ Update : Troll class : Function
    dropItem 함수를 정의했습니다.

--- ToH/ToH/Troll.cpp
+++ ToH/ToH/Troll.cpp
@@ -1,15 +1,19 @@
 #include "Troll.h"
 #include <random>
+#include "TrollDropItem.h"
+#include <algorithm>  // max() 함수 사용을 위해 필요
+
+using namespace std;
 
 Troll::Troll(int level)
 {
 	random_device rd;
-	uniform_int_distribution<int> randomHealth(20, 30);
-	uniform_int_distribution<int> randomAttack(5, 10);
+	uniform_int_distribution<int> randomHealth(20, 30);	// 체력 랜덤 범위
+	uniform_int_distribution<int> randomAttack(5, 10);	// 공격력 랜덤 범위
 
 	static int count = 1;
 
-	name = "troll" + to_string(count++);
+	name = "Troll" + to_string(count++);
 	health = level * randomHealth(rd);
 	attack = level * randomAttack(rd);
 }
@@ -31,6 +35,11 @@ int Troll::getAttack() const
 
 void Troll::takeDamage(int damage)
 {
-	health -= damage;
-	health = health < 0 ? 0 : health;
+	health = max(0, health - damage);
+}
+
+Item* Troll::dropItem() const
+{
+	Item* drop = new TrollDropItem();
+	return drop;
 }
