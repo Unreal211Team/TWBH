e7f0923ae6da4da188aa04c3933913adb80b2cdc
Minwoo <ymw1023@kakao.com>
2025-01-13 17:08:43 +0900

    ✨Update : AttackBoost class : Functions
    디폴트 매개인자를 갖는 생성자가 생겼습니다.
    getName 함수를 정의했습니다.
    canUse 함수를 정의했습니다.
    use 함수를 정의했습니다.
    getPrice 함수를 정의했습니다.

--- ToH/ToH/AttackBoost.cpp
+++ ToH/ToH/AttackBoost.cpp
@@ -1,1 +1,32 @@
 #include "AttackBoost.h"
+#include "Character.h"
+#include <iostream>
+
+using namespace std;
+
+AttackBoost::AttackBoost(string name, bool canUsing, int price, int attackIncrease)
+	: name(name), canUsing(canUsing), price(price), attackIncrease(attackIncrease)
+{
+}
+
+string AttackBoost::getName() const
+{
+	return name;
+}
+
+bool AttackBoost::canUse() const
+{
+	return canUsing;
+}
+
+void AttackBoost::use(Character* character) const
+{
+	cout << "공격력이 상승합니다." << endl;
+	int attack = character->getAttack() + attackIncrease;
+	character->setAttack(attack);
+}
+
+int AttackBoost::getPrice() const
+{
+	return price;
+}
--- ToH/ToH/AttackBoost.h
+++ ToH/ToH/AttackBoost.h
@@ -9,4 +9,11 @@ private:
 	const bool canUsing;		// 아이템 사용 가능 여부
 	const int price;
 	const int attackIncrease;	// 공격력 증가량
+
+public:
+	AttackBoost(std::string name = "AttackBoost", bool canUsing = true, int price = 50, int attackIncrease = 10);
+	std::string getName() const override;
+	bool canUse() const override;
+	void use(Character* character) const override;
+	int getPrice() const override;
 };
