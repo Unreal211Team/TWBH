26a288ffa30e4224539669087485f5a13675871d
Minwoo <ymw1023@kakao.com>
2025-01-13 16:54:57 +0900

    ✨ Update : GoblinDropItem class : Functions
    디폴트 매개인자를 갖는 생성자가 생겼습니다.
    getName 함수를 정의했습니다.
    canUse 함수를 정의했습니다.
    use 함수를 정의했습니다.
    getPrice 함수를 정의했습니다.

--- ToH/ToH/GoblinDropItem.cpp
+++ ToH/ToH/GoblinDropItem.cpp
@@ -1,1 +1,29 @@
 #include "GoblinDropItem.h"
+#include "Character.h"
+
+using namespace std;
+
+GoblinDropItem::GoblinDropItem(string name, bool canUsing, int price)
+	: name(name), canUsing(canUsing), price(price)
+{
+}
+
+string GoblinDropItem::getName() const
+{
+	return name;
+}
+
+bool GoblinDropItem::canUse() const
+{
+	return canUsing;
+}
+
+void GoblinDropItem::use(Character* character) const
+{	// 사용가능한 아이템이 아님
+	return;
+}
+
+int GoblinDropItem::getPrice() const
+{
+	return price;
+}
--- ToH/ToH/GoblinDropItem.h
+++ ToH/ToH/GoblinDropItem.h
@@ -8,4 +8,11 @@ private:
 	const std::string name;
 	const bool canUsing;	// 아이템 사용 가능 여부
 	const int price;
+
+public:
+	GoblinDropItem(std::string name = "부서진 고블린의 창", bool canUsing = false, int price = 200);
+	std::string getName() const override;
+	bool canUse() const override;
+	void use(Character* character) const override;
+	int getPrice() const override;
 };
