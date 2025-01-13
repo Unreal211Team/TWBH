f16438c2d998d2fd63033d847e7a795f7783f82e
Minwoo <ymw1023@kakao.com>
2025-01-13 17:20:39 +0900

    ✨ Update : BossMonster class : Functions
    디폴트 매개인자를 갖는 생성자가 생겼습니다.
    getName 함수를 정의했습니다.
    getHealth 함수를 정의했습니다.
    getAttack 함수를 정의했습니다.
    takeDamage 함수를 정의했습니다.
    dropItem 함수를 정의했습니다.

--- ToH/ToH/BossMonster.cpp
+++ ToH/ToH/BossMonster.cpp
@@ -1,1 +1,43 @@
 #include "BossMonster.h"
+#include <random>
+#include "BossMonsterDropItem.h"
+
+using namespace std;
+
+BossMonster::BossMonster(int level)
+{
+	random_device rd;
+	uniform_int_distribution<int> randomHealth(50, 60);
+	uniform_int_distribution<int> randomAttack(5, 10);
+
+	name = "Dragon";
+	health = level * randomHealth(rd);
+	attack = level * randomAttack(rd);
+}
+
+string BossMonster::getName() const
+{
+	return name;
+}
+
+int BossMonster::getHealth() const
+{
+	return health;
+}
+
+int BossMonster::getAttack() const
+{
+	return attack;
+}
+
+void BossMonster::takeDamage(int damage)
+{
+	health -= damage;
+	health = health < 0 ? 0 : health;
+}
+
+Item* BossMonster::dropItem() const
+{
+	Item* drop = new BossMonsterDropItem();
+	return drop;
+}
--- ToH/ToH/BossMonster.h
+++ ToH/ToH/BossMonster.h
@@ -8,4 +8,12 @@ private:
 	std::string name;
 	int health;
 	int attack;
+
+public:
+	BossMonster(int level = 10);
+	std::string getName() const override;
+	int getHealth() const override;
+	int getAttack() const override;
+	void takeDamage(int damage) override;
+	Item* dropItem() const override;
 };
