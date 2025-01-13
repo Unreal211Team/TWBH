4da3c04624bb8889fce492a4cfcedca493eed46b
Minwoo <ymw1023@kakao.com>
2025-01-13 17:37:20 +0900

    ✨ Update : BossMonsterDropItem class : Functions
    디폴트 매개인자를 갖는 생성자가 생겼습니다.
    getName 함수를 정의했습니다.
    canUse 함수를 정의했습니다.
    use 함수를 정의했습니다.
    getPrice 함수를 정의했습니다.

--- ToH/ToH/BossMonsterDropItem.cpp
+++ ToH/ToH/BossMonsterDropItem.cpp
@@ -1,1 +1,40 @@
 #include "BossMonsterDropItem.h"
+#include "Character.h"
+#include <iostream>
+
+using namespace std;
+
+BossMonsterDropItem::BossMonsterDropItem(string name, bool canUsing, int price)
+	: name(name), canUsing(canUsing), price(price)
+{
+}
+
+std::string BossMonsterDropItem::getName() const
+{
+	return name;
+}
+
+bool BossMonsterDropItem::canUse() const
+{
+	return canUsing;
+}
+
+void BossMonsterDropItem::use(Character* character) const
+{	
+	cout << "드래곤 하트를 복용합니다." << endl;
+	cout << "체력이 100배 상승합니다." << endl;
+	cout << "공격력이 100배 상승합니다." << endl;
+
+	// 먼저 공격력과 최대 체력 증가
+	character->setAttack(character->getAttack() * 100);
+	character->setMaxHealth(character->getMaxHealth() * 100);
+	// 그 후에 현재 체력 업데이트
+	character->setHealth(character->getMaxHealth());
+
+	return;
+}
+
+int BossMonsterDropItem::getPrice() const
+{
+	return price;
+}
--- ToH/ToH/BossMonsterDropItem.h
+++ ToH/ToH/BossMonsterDropItem.h
@@ -8,4 +8,11 @@ private:
 	const std::string name;
 	const bool canUsing;
 	const int price;
+
+public:
+	BossMonsterDropItem(std::string name = "µå·¡°ï ÇÏÆ®", bool canUsing = true, int price = 10000000);
+	std::string getName() const override;
+	bool canUse() const override;
+	void use(Character* character) const override;
+	int getPrice() const override;
 };
