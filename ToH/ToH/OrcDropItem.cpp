fe085b1d4c4d31e820f04b1b0141c00c0b34b6a6
Minwoo <ymw1023@kakao.com>
2025-01-13 16:59:26 +0900

    ✨ Update : OrcDropItem class : Functions
    디폴트 매개인자를 갖는 생성자가 생겼습니다.
    getName 함수를 정의했습니다.
    canUse 함수를 정의했습니다.
    use 함수를 정의했습니다.
    getPrice 함수를 정의했습니다.

--- ToH/ToH/OrcDropItem.cpp
+++ ToH/ToH/OrcDropItem.cpp
@@ -1,1 +1,29 @@
 #include "OrcDropItem.h"
+#include "Character.h"
+
+using namespace std;
+
+OrcDropItem::OrcDropItem(string name, bool canUsing, int price)
+	: name(name), canUsing(canUsing), price(price)
+{
+}
+
+std::string OrcDropItem::getName() const
+{
+	return name;
+}
+
+bool OrcDropItem::canUse() const
+{
+	return canUsing;
+}
+
+void OrcDropItem::use(Character* character) const
+{	// 사용가능한 아이템이 아님
+	return;
+}
+
+int OrcDropItem::getPrice() const
+{
+	return price;
+}
--- ToH/ToH/OrcDropItem.h
+++ ToH/ToH/OrcDropItem.h
@@ -8,4 +8,11 @@ private:
 	const std::string name;
 	const bool canUsing;
 	const int price;
+
+public:
+	OrcDropItem(std::string name = "부서진 오크의 망치", bool canUsing = false, int price = 200);
+	std::string getName() const override;
+	bool canUse() const override;
+	void use(Character* character) const override;
+	int getPrice() const override;
 };
