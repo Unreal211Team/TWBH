b8cec7bfd61588636b35617109578b2a9352778a
Minwoo <ymw1023@kakao.com>
2025-01-13 16:31:35 +0900

    ✨ Update : Character class : Functions
    디폴트 매개인자를 갖는 생성자가 생겼습니다.
    복사 생성자를 제거했습니다.
    대입 연산자를 제거했습니다.
    getInstance 함수가 생겼습니다.
    displayStatus 함수가 생겼습니다.
    getName 함수가 생겼습니다.
    getLevel 함수가 생겼습니다.
    getHealth 함수가 생겼습니다.
    setHealth 함수가 생겼습니다.
    getMaxHealth 함수가 생겼습니다.
    setMaxHealth 함수가 생겼습니다.
    getAttack 함수가 생겼습니다.
    setAttack 함수가 생겼습니다.
    getExperience 함수가 생겼습니다.
    getMaxExperience 함수가 생겼습니다.
    addExperience 함수가 생겼습니다.
    getGold 함수가 생겼습니다.
    addGold 함수가 생겼습니다.
    takeDamage 함수가 생겼습니다.
    getInventory 함수가 생겼습니다.
    IsLevelUp 함수가 생겼습니다.
    addItem 함수가 생겼습니다.
    useItem 함수가 생겼습니다.
    소멸자가 생겼습니다.

--- ToH/ToH/Character.cpp
+++ ToH/ToH/Character.cpp
@@ -1,1 +1,172 @@
 #include "Character.h"
+#include "Item.h"
+#include <iostream>
+
+using namespace std;
+
+Character* Character::instance = nullptr;
+
+Character::Character(string name) : name(name)
+{
+	level = 1;
+	health = maxHealth = 200;
+	attack = 30;
+	experience = 0;
+	maxExperience = 100;
+	gold = 0;
+}
+
+Character* Character::getInstance(const string& name)
+{
+	if (instance == nullptr)
+	{
+		instance = new Character(name);
+	}
+
+	return instance;
+}
+
+void Character::displayStatus() const
+{
+	cout << " --- status ---" << endl;
+	cout << "  " << name << endl;
+	cout << " Lv." << level << "	(" << experience << "/" << maxExperience << ")" << endl;
+	cout << " HP	" << health << "/" << maxHealth << endl;
+	cout << " 공격력	" << attack << endl;
+}
+
+string Character::getName() const
+{
+	return name;
+}
+
+int Character::getLevel() const
+{
+	return level;
+}
+
+int Character::getHealth() const
+{
+	return health;
+}
+
+void Character::setHealth(int health)
+{
+	this->health = max(health, 0);
+}
+
+int Character::getMaxHealth() const
+{
+	return maxHealth;
+}
+
+void Character::setMaxHealth(int maxHealth)
+{
+	this->maxHealth = maxHealth;
+}
+
+int Character::getAttack() const
+{
+	return attack;
+}
+
+void Character::setAttack(int attack)
+{
+	this->attack = attack;
+}
+
+int Character::getExperience() const
+{
+	return experience;
+}
+
+int Character::getMaxExperience() const
+{
+	return maxExperience;
+}
+
+void Character::addExperience(int experience)
+{
+	this->experience += experience;
+}
+
+int Character::getGold() const
+{
+	return gold;
+}
+
+void Character::addGold(int gold)
+{
+	this->gold += gold;
+}
+
+void Character::takeDamage(int damage)
+{
+	setHealth(this->health - damage);
+}
+
+vector<Item*>& Character::getInventory()
+{
+	return inventory;
+}
+
+bool Character::IsLevelUp() const
+{
+	return experience >= maxExperience;
+}
+
+void Character::levelUp()
+{
+	// 경험치 감소
+	experience -= maxExperience;
+
+	// 요구 경험치 증가
+	maxExperience *= 1.2;
+	maxExperience = (maxExperience / 10) * 10;  // 10의 배수로 설정
+
+	// 레벨업
+	++level;
+
+	// 체력업
+	maxHealth += 20;
+
+	// 완전회복
+	health = maxHealth;
+
+	// 공격업
+	attack += 5;
+}
+
+void Character::addItem(Item* item)
+{
+	inventory.push_back(item);
+}
+
+void Character::useItem(int index)
+{
+	// 재료 아이템 사용 불가
+	if (!inventory[index]->canUse())
+	{
+		cout << "사용 불가능한 아이템입니다." << endl;
+		return;
+	}
+
+	cout << inventory[index]->getName() << "을 사용합니다." << endl;
+
+	inventory[index]->use(this);
+
+	// 메모리 해제 후 erase
+	delete inventory[index];
+
+	inventory.erase(inventory.begin() + index);
+}
+
+Character::~Character()
+{
+	// 메모리 해제
+	for (Item* item : inventory)
+	{
+		delete item;  // 아이템에 대한 메모리 해제
+	}
+	inventory.clear();  // 벡터 비우기
+}
--- ToH/ToH/Character.h
+++ ToH/ToH/Character.h
@@ -17,4 +17,32 @@ private:
 	int maxExperience;
 	int gold;
 	std::vector<Item*> inventory;
+
+	Character(std::string name);
+	Character(const Character&) = delete;
+	Character& operator=(const Character&) = delete;
+
+public:
+	static Character* getInstance(const std::string& name = "");
+	void displayStatus() const;
+	std::string getName() const;
+	int getLevel() const;
+	int getHealth() const;
+	void setHealth(int health);
+	int getMaxHealth() const;
+	void setMaxHealth(int maxHealth);
+	int getAttack() const;
+	void setAttack(int attack);
+	int getExperience() const;
+	int getMaxExperience() const;
+	void addExperience(int experience);
+	int getGold() const;
+	void addGold(int gold);
+	void takeDamage(int damage);
+	std::vector<Item*>& getInventory();
+	bool IsLevelUp() const;
+	void levelUp();
+	void addItem(Item* item);
+	void useItem(int index);
+	~Character();
 };
