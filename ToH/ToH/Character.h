dd16cface8f6b2d63f1c8381f22f8d7e74a5c08f
Minwoo <ymw1023@kakao.com>
2025-01-13 16:23:03 +0900

    ✨ Update : Character class : Variables
    instance 싱글톤 변수가 생겼습니다.
    name 변수가 생겼습니다.
    level 변수가 생겼습니다.
    health 변수가 생겼습니다.
    maxhealth 변수가 생겼습니다.
    attack 변수가 생겼습니다.
    experience 변수가 생겼습니다.
    maxExperience 변수가 생겼습니다.
    gold 변수가 생겼습니다.
    inventory 변수가 생겼습니다.

--- ToH/ToH/Character.h
+++ ToH/ToH/Character.h
@@ -1,6 +1,20 @@
 #pragma once
 
+#include <string>
+#include <vector>
+class Item;
+
 class Character
 {
-
+private:
+	static Character* instance;
+	const std::string name;
+	int level;
+	int health;
+	int maxHealth;
+	int attack;
+	int experience;
+	int maxExperience;
+	int gold;
+	std::vector<Item*> inventory;
 };
