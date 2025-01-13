a07c73092b2f6eb74d1c486bb6f8a8b7b106a577
Minwoo <ymw1023@kakao.com>
2025-01-13 17:02:59 +0900

    ✨Update : TrollDropItem class : Functions
    디폴트 매개인자를 갖는 생성자가 생겼습니다.
    getName 함수를 정의했습니다.
    canUse 함수를 정의했습니다.
    use 함수를 정의했습니다.
    getPrice 함수를 정의했습니다.

--- ToH/ToH/TrollDropItem.cpp
+++ ToH/ToH/TrollDropItem.cpp
@@ -1,1 +1,29 @@
 #include "TrollDropItem.h"
+#include "Character.h"
+
+using namespace std;
+
+TrollDropItem::TrollDropItem(string name, bool canUsing, int price)
+	: name(name), canUsing(canUsing), price(price)
+{
+}
+
+std::string TrollDropItem::getName() const
+{
+	return name;
+}
+
+bool TrollDropItem::canUse() const
+{
+	return canUsing;
+}
+
+void TrollDropItem::use(Character* character) const
+{	// 사용가능한 아이템이 아님
+	return;
+}
+
+int TrollDropItem::getPrice() const
+{
+	return price;
+}
--- ToH/ToH/TrollDropItem.h
+++ ToH/ToH/TrollDropItem.h
@@ -8,4 +8,11 @@ private:
 	const std::string name;
 	const bool canUsing;
 	const int price;
+
+public:
+	TrollDropItem(std::string name = "ºÎ¼­Áø Æ®·ÑÀÇ ¸ùµÕÀÌ", bool canUsing = false, int price = 200);
+	std::string getName() const override;
+	bool canUse() const override;
+	void use(Character* character) const override;
+	int getPrice() const override;
 };
