0d2329a34ec4f775488788e8dbb5dcad5346078d
Minwoo <ymw1023@kakao.com>
2025-01-13 16:44:24 +0900

    ✨ Update : Item class : Functions
    getName 함수가 생겼습니다.
    cauUse 함수가 생겼습니다.
    use 함수가 생겼습니다.
    getPrice 함수가 생겼습니다.
    가상 소멸자가 생겼습니다.

--- ToH/ToH/Item.h
+++ ToH/ToH/Item.h
@@ -1,6 +1,15 @@
 #pragma once
 
+#include <string>
+
+class Character;
+
 class Item
 {
-	
+public:
+	virtual	std::string getName() const = 0;
+	virtual bool canUse() const = 0;
+	virtual	void use(Character* character) const = 0;
+	virtual	int getPrice() const = 0;
+	virtual ~Item() = default;
 };
