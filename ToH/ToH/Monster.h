4a52a2eb4a855fd3c3e7d55cd56fff3eaf021726
Minwoo <ymw1023@kakao.com>
2025-01-13 16:35:26 +0900

    ✨ Update : Monster class : Function
    dropItem 함수가 생겼습니다.

--- ToH/ToH/Monster.h
+++ ToH/ToH/Monster.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #include <string>
-
-using namespace std;
+#include "Item.h"
 
 class Monster
 {
 public:
-	virtual string	getName() const = 0;
-	virtual int		getHealth() const = 0;
-	virtual	int		getAttack() const = 0;
-	virtual void	takeDamage(int damage) = 0;
+	virtual std::string getName() const = 0;
+	virtual int getHealth() const = 0;
+	virtual	int getAttack() const = 0;
+	virtual void takeDamage(int damage) = 0;
+	virtual	Item* dropItem() const = 0;
 };
