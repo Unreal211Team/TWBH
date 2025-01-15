ac5b5635473c0f4fda15f6a4cc47e858e43ce2e7
wonho-seo <63235720+wonho-seo@users.noreply.github.com>
2025-01-15 19:03:27 +0900

    add GameManger AttackModeMonsterFactory
    add GameManger AttackModeMonsterFactory

--- ToH/ToH/GameManager.cpp
+++ ToH/ToH/GameManager.cpp
@@ -7,6 +7,7 @@
 #include "Troll.h"
 #include "Slime.h"
 #include "Skill.h"
+#include "AttackModeMonsterFactory.h"
 
 GameManager* GameManager::instance = nullptr;
 
@@ -160,11 +161,15 @@ void GameManager::battle(Character* player, Monster* monster)
 				return;
 			}
 
+			AttackModeMonsterFactory& attackModeMonsterFactory = AttackModeMonsterFactory::getInstance();
+			shared_ptr<ActingMonster> tempMonster = attackModeMonsterFactory.getRandomMonster(*monster);
 
 			// 몬스터의 공격
 			cout << monster->getName() << "이(가) ";
-			player->takeDamage(monster->getAttack());
+			player->takeDamage(tempMonster->getAttack());
+			tempMonster->doAttack();
 
+			cout << "플레이어의 체력 :" << player->getHealth() << endl;
 			// 몬스터의 공격에 플레이어가 사망했는지 확인하는 로직
 			if (player->getHealth() == 0)
 			{
