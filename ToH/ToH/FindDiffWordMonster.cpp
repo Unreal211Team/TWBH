85b3885c7db01612bd3412d9bbce23be61700ed2
wonho-seo <63235720+wonho-seo@users.noreply.github.com>
2025-01-15 15:13:50 +0900

    fix FindDiffWordMonster(find pos-> find count)
    fix: FindDiffWordMonster
    find pos-> find count

--- ToH/ToH/FindDiffWordMonster.cpp
+++ ToH/ToH/FindDiffWordMonster.cpp
@@ -1,5 +1,5 @@
 #include "FindDiffWordMonster.h"
-
+using namespace std;
 int FindDiffWordMonster::getAttack() const
 {
     return this->damage;
@@ -10,20 +10,36 @@ void FindDiffWordMonster::doAttack()
     Character* character = Character::getInstance("");
 
     random_device rd;
-    uniform_int_distribution<int> random(0, Lstring.size() - 1);
+    uniform_int_distribution<int> randomCol(0, Lstring.size() * 3);
+    uniform_int_distribution<int> randomRow(1, character->getLevel());
     string str;
     string tempString{ Lstring };
-    int changePos = random(rd);
-    tempString[changePos] = 'i';
-    cout << "ºóÆ´À» Ã£ÀÚ!!" << endl;
-    cout << tempString << endl;
-    cout << "iÀÇ À§Ä¡´Â?" << endl;
+    int changeRow = randomRow(rd);
+    
+    int answer = 0;
+
+    cout << "ºóÆ´À» Ã£ÀÚ!!" << endl << endl;
+
+    for (int col = 0; col < character->getLevel(); col++)
+    {
+        int changeCol = randomCol(rd);
+        string tempString(10, 'l'); 
+        if (changeCol < Lstring.size())
+        {
+            tempString[changeCol] = 'i';
+            answer++;
+        }
+        cout << tempString <<endl;
+
+    }
+
+    cout << "iÀÇ °¹¼ö´Â?" << endl;
     clock_t start = clock();
     cin >> str;
     clock_t end = clock();
 
     double spendTime = static_cast<double>(end - start) / CLOCKS_PER_SEC;
-    if (spendTime < timeOut && to_string(changePos + 1) == str)
+    if (spendTime < timeOut && to_string(answer) == str)
     {
         cout << "ºóÆ´À» ÅëÇØ È¸ÇÇÇß´Ù!!" << endl;
         this->damage = 0;
